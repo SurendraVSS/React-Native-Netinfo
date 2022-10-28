1816d0234b74deac21602ad3d4cbc759b98e81ac
SurendraVSS <106951672+SurendraVSS@users.noreply.github.com>
2022-10-28 19:56:12 +0530

    hello

--- screens/NoConnectionScreen.js
+++ screens/NoConnectionScreen.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View,Button, Image } from 'react-native';
-
+//hello
 const NoConnectionScreen = (props) => {
   return (
     <View style={styles.container}>
